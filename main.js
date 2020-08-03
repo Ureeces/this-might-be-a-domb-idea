474e4014e528b68b51a0e27f74ee443867ed4b73
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-08-03 15:53:37 -0400

    Fixed heading confusion. Finished list and 1st image styles.

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+// heading
+const heading = document.querySelector("h1");
+heading.style.fontSize = "5em";
+
+// paragraph
 const lorem = document.querySelector("p");
 lorem.style.color = "blue";
-lorem.style.fontSize = "5em";
 lorem.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+// list
+const thirteen = document.querySelector("#item-13");
+thirteen.style.opacity = 0.5;
+
+const three = document.querySelector("#item-3");
+three.innerText = "I say, Hi!";
+
+// img
+const image = document.querySelector("img");
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+image.height = "300";
